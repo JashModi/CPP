17f5c9763772ec7d9ac5395db8d7130d35e083e8
JashModi <59430686+JashModi@users.noreply.github.com>
2020-01-22 12:10:16 +0530

    complete program

--- jashacc.cpp
+++ jashacc.cpp
@@ -3,20 +3,35 @@
 using namespace std;
 class bank
 {
-public:
-    string bankname;
+    string bankname="BOB";
     long accountnum;
     double balance;
-
+public:
     void banksetter()
     {
-
-        cout<<"Enter bank name";
-        getline(cin,bankname);
+        //cout<<"Enter bank name";
+        //cin.get();
+        //getline(cin,bankname);
         cout<<"Enter Account number";
         cin>>accountnum;
         cout<<"Enter Account Balance";
         cin>>balance;
+    }
+    void depositBalance(double d)
+    {
+        balance += d;
+    }
+    void withdrawBalance(double d)
+    {
+        balance -= d;
+    }
+    long getAcno()
+    {
+        return accountnum;
+    }
+    double getBalance()
+    {
+        return balance;
     }
 };
 
@@ -29,6 +44,7 @@ public:
     {
 
         cout<<"Enter person name";
+        cin.get();
         getline(cin,personname);
         cout<<"Enter Age";
         cin>>age;
@@ -42,38 +58,94 @@ public:
     void deposit()
     {
 
-        int deposit;
+        double deposit;
         cout<<"Enter the amount Deposited"<<endl;
         cin>>deposit;
-        balance+=deposit;
-        cout<<balance;
-
+        if(deposit>0)
+        {
+            depositBalance(deposit);
+            cout<<getBalance();
+        }
+        else
+        {
+            cout<<"Invalid";
+        }
     }
     void withdraw()
     {
 
-        int withdraw;
+        double withdraw;
         cout<<"Enter the amount Withdrawn"<<endl;
         cin>>withdraw;
-        balance+=withdraw;
-        cout<<balance;
+        if(withdraw>getBalance()||withdraw<0)
+        {
 
+            cout<<"Invalid Amount Try Again";
+        }
+        else
+        {
+            withdrawBalance(withdraw);
+            cout<<getBalance();
+        }
     }
 
 };
 
 main()
 {
+    int tacno;
+    char yes;
+    char ops;
+    char ask;
 
-    customer c;
-    c.banksetter();
-    c.bankname;
-    c.accountnum;
-    c.balance;
-    c.personalsetter();
-    c.personname;
-    c.age;
-    c.deposit();
-    c.withdraw();
+    customer c[3];
+    for(int i=0; i<3; i++)
+    {
+        c[i].banksetter();
+        c[i].personalsetter();
+    }
+
+    do
+    {
+        cout<<"Enter ac no.:\n";
+        cin>>tacno;
+        bool flag = false;
+        for(int i=0; i<3 ; i++)
+        {
+            if(c[i].getAcno() == tacno)
+            {
+                flag=true;
+                do
+                {
+                    cout<<"What you want to do ?"<<"\n"<<"1. deposit"<<"\n"<<"2. Withdraw"<<endl;
+                    cin>>ops;
+                    switch(ops)
+                    {
+                    case'1':
+                            c[i].deposit();
+                        break;
+                    case'2':
+                            c[i].withdraw();
+                        break;
+                    default:
+                        cout<<"Invalid";
+                    }
+                    cout<<"Do you want to continue ?"<<"\n"<<"Press Y or y to continue"<<endl;
+                    cin>>yes;
+                }
+                while(yes=='y'||yes=='Y');
+            }
+
+        }
+        if(!flag)
+        {
+            cout<<"NO ac No found";
+        }
+        cout<<"Do You want to continue Another session ?"<<endl;
+        cin>>ask;
+    }
+    while(ask=='y'||ask=='Y');
+
+
 
 }
